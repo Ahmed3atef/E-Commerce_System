86641bfb40921416b4b17077e92b96f5353b33c1
Ahmed@Atef <clay674302@gmail.com>
2025-12-15 23:54:56 +0200

    refactor: Replace UserViewSet with MeView for current user profile management and update API routing.

--- account/auth/views.py
+++ account/auth/views.py
@@ -2,8 +2,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
-from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin
-from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer, RegisterResponseSerializer, RegisterSerializer, UserSerializer
 from django.contrib.auth import get_user_model
@@ -12,7 +10,11 @@ from drf_spectacular.utils import extend_schema
 User = get_user_model()
 
 class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
+    @extend_schema(
+        responses={200: CustomTokenObtainPairSerializer}
+    )
+    def get_serializer_class(self):
+        return CustomTokenObtainPairSerializer
 
 class RegisterView(APIView):
     http_method_names = ['post']
@@ -33,9 +35,31 @@ class RegisterView(APIView):
             status=status.HTTP_201_CREATED
         )
         
-class UserViewSet(RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
-    http_method_names = ['get', 'put', 'patch']
-    serializer_class = UserSerializer
+class MeView(APIView):
     
-    def get_queryset(self):
-        return User.objects.filter(email=self.request.user.email)
+    @extend_schema(
+        responses={200: UserSerializer}
+    )
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+    
+    @extend_schema(
+        request=UserSerializer,
+        responses={200: UserSerializer}
+    )
+    def put(self, request):
+        serializer = UserSerializer(request.user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    @extend_schema(
+        request=UserSerializer,
+        responses={200: UserSerializer}
+    )
+    def patch(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,6 @@
-from django.urls import path,include
-from account.auth.views import CustomTokenObtainPairView, RegisterView
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenBlacklistView
-from rest_framework.routers import DefaultRouter
-from account.auth.views import UserViewSet
-# from account.views import SellerAdminViewSet, UserViewSet, MeView
-
-
-router = DefaultRouter()
-router.register("user", UserViewSet, basename="user")
+from django.urls import path
+from account.auth.views import CustomTokenObtainPairView, RegisterView, MeView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenBlacklistView
 
 
 urlpatterns = [
@@ -15,5 +8,5 @@ urlpatterns = [
     path("auth/login/", CustomTokenObtainPairView.as_view(), name="login"),
     path("auth/logout/", TokenBlacklistView.as_view(), name="logout"),
     path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("", include(router.urls))
+    path("users/me/", MeView.as_view(), name="user-me"),
 ]
